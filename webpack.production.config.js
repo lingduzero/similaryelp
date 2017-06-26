74d31585c8bfed0ac2f73c481120f642bbaf4db7
lingduzero <bz63@njit.edu>
2017-06-26 16:24:59 -0400

    deploy change

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,84 +1,77 @@
-var path = require('path')
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const webpack = require('webpack')
+const path = require('path')
+const pkg = require('./package.json')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
-  entry: {
-    app: path.resolve(__dirname, 'app/index.jsx'),
-    // 将 第三方依赖 单独打包
-    vendor: [
-      'react',
-      'react-dom',
-      'react-redux',
-      'react-router',
-      'redux',
-      'es6-promise',
-      'whatwg-fetch',
-      'immutable'
+    entry: {
+        app: './app/index.jsx',
+        vendor: Object.keys(pkg.dependencies)
+    },
+    output: {
+        filename: './build/dist/js/[name].[chunkhash:8].js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        loaders: [{
+                test: /\.(js|jsx)?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /(node_modules)/,
+                loader: ExtractTextPlugin.extract(['css-loader'])
+            }, {
+                test: /\.less$/,
+                exclude: /(node_modules)/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'less-loader'])
+            },
+            {
+                test: /\.(png|gif||jpg|jpeg|bmp)($|\?)/i,
+                exclude: /(node_modules)/,
+                loader: 'url-loader?limit=100000&name=imgs/[name].one.[ext]'
+            },
+            {
+                test: /\.(png|gif|svg|ttf|eot|woff|woff2)($|\?)/i,
+                exclude: /(node_modules)/,
+                loader: 'url-loader?limit=100000&name=fonts/[name].one.[ext]'
+            },
+
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin("copyright:lllyyy2012@icloud.com"),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: './build/dist/js/[name].[chunkhash:8].js'
+        }),
+        new HtmlWebpackPlugin({
+            template: './app/index.tmpl.html',
+            filename: './build/dist/index.html'
+        }),
+        new webpack.DefinePlugin({
+            __DEV__: false
+
+
+        }),
+        //   new webpack.DefinePlugin({
+        //     'process.env': {
+        //     'NODE_ENV':'production'
+        //     }
+        // }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new ExtractTextPlugin('./build/dist/css/[name].[chunkhash:8].css'),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+
     ]
-  },
-  output: {
-    path: __dirname + "/build",
-    filename: "[name].[chunkhash:8].js",
-    publicPath: '/'
-  },
-
-  resolve:{
-      extensions:['', '.js','.jsx']
-  },
-
-  module: {
-    loaders: [
-        { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
-        { test: /\.less$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss!less') },
-        { test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss') },
-        { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000&name=img/[name].[chunkhash:8].[ext]' },
-        { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[chunkhash:8].[ext]'}
-    ]
-  },
-  postcss: [
-    require('autoprefixer')
-  ],
-
-  plugins: [
-    // webpack 内置的 banner-plugin
-    new webpack.BannerPlugin("Copyright by bingbing zhu."),
-
-    // html 模板插件
-    new HtmlWebpackPlugin({
-        template: __dirname + '/app/index.tmpl.html'
-    }),
-
-    // 定义为生产环境，编译 React 时压缩到最小
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-
-    // 为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，并为它们分配最小的ID
-    new webpack.optimize.OccurenceOrderPlugin(),
-
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          //supresses warnings, usually from module minification
-          warnings: false
-        }
-    }),
-
-    // 分离CSS和JS文件
-    new ExtractTextPlugin('[name].[chunkhash:8].css'),
-
-    // 提供公共代码
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: '[name].[chunkhash:8].js'
-    }),
-
-    // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
-    new webpack.DefinePlugin({
-      __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
-    })
-  ]
 }
